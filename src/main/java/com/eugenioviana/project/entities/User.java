187a00bf8acab3999c22d90ca524c54867b1b9ec
Eugenio Viana <eugenioviana.developer@gmail.com>
2025-02-20 15:02:46 -0300

    Order Model

--- src/main/java/com/eugenioviana/project/entities/User.java
+++ src/main/java/com/eugenioviana/project/entities/User.java
@@ -1,12 +1,15 @@
 package com.eugenioviana.project.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,10 +26,12 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
 	public User(Long id, String name, String email, String phone, String password) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -35,6 +40,10 @@ public class User implements Serializable{
 	}
 
 
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 	public String getPhone() {
 		return phone;
 	}
